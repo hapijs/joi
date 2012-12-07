e92a97dcb5e40e46ccf71613ddab7c5ef92eee2e
Van Nguyen <thegoleffect@gmail.com>
2012-12-07 10:48:51 -0800

    100% coverage

--- test/index.js
+++ test/index.js
@@ -51,6 +51,74 @@ describe('#validate', function () {
             done();
         });
     });
+    
+    it('should skip functions if skipFunctions is set to true', function (done) {
+        var prev = Joi.settings.skipFunctions;
+        Joi.settings.skipFunctions = true;
+        
+        var obj = {
+            a: 10,
+            b: 'a',
+            c: 'joe@example.com',
+            test: function(){}
+        };
+
+        Joi.validate(obj, config, function (err) {
+
+            Joi.settings.skipFunctions = prev;
+            expect(err).to.exist;
+            done();
+        });
+    });
+    
+    it('should report multiple unspecified inputs using plural grammar', function (done) {
+
+        var obj = {
+            a: 10,
+            b: 'a',
+            c: 'joe@example.com',
+            d: 'ohai',
+            e: 'there'
+        };
+
+        Joi.validate(obj, config, function (err) {
+
+            expect(err).to.exist;
+            done();
+        });
+    });
+    
+    it('should return error using saveConversions', function (done) {
+
+        var prev = Joi.settings.saveConversions;
+        Joi.settings.saveConversions = true;
+        
+        var obj = {
+            a: 10,
+            b: 'a',
+            c: 'joe@example.com'
+        };
+
+        Joi.validate(obj, config, function (err) {
+
+            Joi.settings.saveConversions = prev;
+            expect(err).to.exist;
+            done();
+        });
+    });
+    
+    it('should return error on zero matching and one non-matching key', function (done) {
+
+        var obj = {
+            d: 1000
+        };
+
+        Joi.validate(obj, config, function (err) {
+
+            expect(err).to.exist;
+            done();
+        });
+    });
 });
 
 
