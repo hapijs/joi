ce33b98ac640021f234fbbf21d2f1f2fc9b7526d
Daniel Bretoi <daniel@bretoi.com>
2014-07-23 16:02:33 -0700

    path for array includes to contain key

--- test/errors.js
+++ test/errors.js
@@ -140,7 +140,7 @@ describe('errors', function () {
         schema.validate(input, function (err, value) {
 
             expect(err).to.exist;
-            expect(err.details[0].path).to.equal('1');
+            expect(err.details[0].path).to.equal('1.1.x');
             done();
         });
     });
@@ -156,7 +156,7 @@ describe('errors', function () {
         schema.validate(input, function (err, value) {
 
             expect(err).to.exist;
-            expect(err.details[0].path).to.equal('1');
+            expect(err.details[0].path).to.equal('1.1');
             done();
         });
     });
@@ -174,7 +174,7 @@ describe('errors', function () {
         Joi.validate(input, schema, function (err, value) {
 
             expect(err).to.exist;
-            expect(err.details[0].path).to.equal('x.1');
+            expect(err.details[0].path).to.equal('x.1.x');
             done();
         });
     });
