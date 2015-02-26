ca2eea88ba64582e11fe47e2892b1a84403984e0
Marsup <marsup@gmail.com>
2015-02-26 21:36:24 +0000

    Change message for object().assert(). Fixes #569.

--- test/object.js
+++ test/object.js
@@ -907,7 +907,7 @@ describe('object', function () {
             schema.validate({ a: { b: 'x', c: 5 }, d: { e: 6 } }, function (err, value) {
 
                 expect(err).to.exist();
-                expect(err.message).to.equal('"value" validation failed because "d.e" failed to equal to a.c');
+                expect(err.message).to.equal('"d.e" validation failed because "d.e" failed to equal to a.c');
 
                 Helper.validate(schema, [
                     [{ a: { b: 'x', c: 5 }, d: { e: 5 } }, true]
@@ -930,7 +930,7 @@ describe('object', function () {
             schema.validate({ a: { b: 'x', c: 5 }, d: { e: 6 } }, function (err, value) {
 
                 expect(err).to.exist();
-                expect(err.message).to.equal('"value" validation failed because "d.e" failed to equal to a.c');
+                expect(err.message).to.equal('"d.e" validation failed because "d.e" failed to equal to a.c');
 
                 Helper.validate(schema, [
                     [{ a: { b: 'x', c: 5 }, d: { e: 5 } }, true]
@@ -991,7 +991,7 @@ describe('object', function () {
             }, function (err) {
 
                 expect(err).to.exist();
-                expect(err.message).to.equal('"value" validation failed because "d.e" failed to pass the assertion test');
+                expect(err.message).to.equal('"d.e" validation failed because "d.e" failed to pass the assertion test');
                 done();
             });
         });
