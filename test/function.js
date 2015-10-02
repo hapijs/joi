9d6013ff264186e61e4389721f51a14f1c0a2275
Eran Hammer <eran@hammer.io>
2015-10-02 10:07:37 -0700

    func with keys test

--- test/function.js
+++ test/function.js
@@ -19,10 +19,26 @@ var it = lab.it;
 
 describe('func', function () {
 
-    it('should validate a function', function (done) {
+    it('validates a function', function (done) {
 
         Helper.validate(Joi.func().required(), [
             [function () { }, true],
+            ['', false]
+        ], done);
+    });
+
+    it('validates a function with keys', function (done) {
+
+        var a = function () { };
+        a.a = 'abc';
+
+        var b = function () { };
+        b.a = 123;
+
+        Helper.validate(Joi.func().keys({ a: Joi.string().required() }).required(), [
+            [function () { }, false],
+            [a, true],
+            [b, false],
             ['', false]
         ], done);
     });
