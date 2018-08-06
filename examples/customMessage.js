4e894c5d3240a322f1984c2d03944c194820abb1
Nicolas Morel <nicolas@morel.io>
2018-08-06 15:58:44 +0200

    Remove stale example. Fixes #1561.

--- examples/customMessage.js
+++ examples/customMessage.js
@@ -13,7 +13,7 @@ const internals = {};
 const schema = Joi.object().options({ abortEarly: false }).keys({
     email: Joi.string().email().required().label('User Email'),
     password: Joi.string().min(8).required(),
-    password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' }, label: 'Password Confirmation' } }).label('This label is not used because language.label takes precedence'),
+    password_confirmation: Joi.any().valid(Joi.ref('password')).required().options({ language: { any: { allowOnly: 'must match password' } } }).label('Password Confirmation'),
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
     company: Joi.string().optional()
