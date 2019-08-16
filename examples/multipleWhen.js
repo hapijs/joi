da4774f8213058fac2da5c00b995c43f68e49787
Eran Hammer <eran@hammer.io>
2019-08-16 00:14:50 +0000

    Replace v16 compatibility interface. Closes #2047

--- examples/multipleWhen.js
+++ examples/multipleWhen.js
@@ -13,10 +13,10 @@ const internals = {};
 const schema = {
     type: Joi.string().required(),
     subtype: Joi.alternatives()
-        .when('type', {is: 'video', then: Joi.valid('mp4', 'wav')})
-        .when('type', {is: 'audio', then: Joi.valid('mp3')})
-        .when('type', {is: 'image', then: Joi.valid('jpg', 'png')})
-        .when('type', {is: 'pdf'  , then: Joi.valid('document')})
+        .when('type', { is: 'video', then: Joi.valid('mp4', 'wav') })
+        .when('type', { is: 'audio', then: Joi.valid('mp3') })
+        .when('type', { is: 'image', then: Joi.valid('jpg', 'png') })
+        .when('type', { is: 'pdf', then: Joi.valid('document') })
 };
 
 
