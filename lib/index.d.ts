3463cf0949e099b4d0bef0663383a7dc2eb051d5
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-09-04 14:13:11 +0300

    fix: remove array from possible values

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -768,7 +768,10 @@ declare namespace Joi {
 
     type ExternalValidationFunction<V = any, R = V> = (value: V, helpers: ExternalHelpers<R>) => R | undefined;
 
-    type SchemaLikeWithoutArray<TSchema = any> = string | number | boolean | null | Schema<TSchema> | SchemaMap<TSchema>;
+    type Primitives = string | number | boolean | bigint | symbol | null;
+
+    type SchemaLikeWithoutArray<TSchema = any> = TSchema extends any[] ? never :
+      (Primitives | Schema<TSchema> | SchemaMap<TSchema>);
     type SchemaLike<TSchema = any> = SchemaLikeWithoutArray<TSchema> | object;
 
     type NullableType<T> = undefined | null | T
@@ -1644,12 +1647,12 @@ declare namespace Joi {
          *
          * @param type - a joi schema object to validate each array item against.
          */
-        items<A>(a: SchemaLike<A>): ArraySchema<A[]>;
-        items<A, B>(a: SchemaLike<A>, b: SchemaLike<B>): ArraySchema<(A | B)[]>;
-        items<A, B, C>(a: SchemaLike<A>, b: SchemaLike<B>, c: SchemaLike<C>): ArraySchema<(A | B | C)[]>;
-        items<A, B, C, D>(a: SchemaLike<A>, b: SchemaLike<B>, c: SchemaLike<C>, d: SchemaLike<D>): ArraySchema<(A | B | C| D)[]>;
-        items<A, B, C, D, E>(a: SchemaLike<A>, b: SchemaLike<B>, c: SchemaLike<C>, d: SchemaLike<D>, e: SchemaLike<E>): ArraySchema<(A | B | C| D | E)[]>;
-        items<A, B, C, D, E , F>(a: SchemaLike<A>, b: SchemaLike<B>, c: SchemaLike<C>, d: SchemaLike<D>, e: SchemaLike<E>, f: SchemaLike<F>): ArraySchema<(A | B | C| D | E |F)[]>;
+        items<A>(a: SchemaLikeWithoutArray<A>): ArraySchema<A[]>;
+        items<A, B>(a: SchemaLikeWithoutArray<A>, b: SchemaLikeWithoutArray<B>): ArraySchema<(A | B)[]>;
+        items<A, B, C>(a: SchemaLikeWithoutArray<A>, b: SchemaLikeWithoutArray<B>, c: SchemaLikeWithoutArray<C>): ArraySchema<(A | B | C)[]>;
+        items<A, B, C, D>(a: SchemaLikeWithoutArray<A>, b: SchemaLikeWithoutArray<B>, c: SchemaLikeWithoutArray<C>, d: SchemaLikeWithoutArray<D>): ArraySchema<(A | B | C| D)[]>;
+        items<A, B, C, D, E>(a: SchemaLikeWithoutArray<A>, b: SchemaLikeWithoutArray<B>, c: SchemaLikeWithoutArray<C>, d: SchemaLikeWithoutArray<D>, e: SchemaLikeWithoutArray<E>): ArraySchema<(A | B | C| D | E)[]>;
+        items<A, B, C, D, E , F>(a: SchemaLikeWithoutArray<A>, b: SchemaLikeWithoutArray<B>, c: SchemaLikeWithoutArray<C>, d: SchemaLikeWithoutArray<D>, e: SchemaLikeWithoutArray<E>, f: SchemaLikeWithoutArray<F>): ArraySchema<(A | B | C| D | E |F)[]>;
         items<TItems>(...types: SchemaLikeWithoutArray<TItems>[]): this;
 
         /**
